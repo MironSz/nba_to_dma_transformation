6a695883ff7b5515f4a2bf605c15b51ebbd5c348
Miron <miron.szewczyk@autonomik.pl>
2019-02-22 17:11:13 +0100

    Added reading NBA from file, testing needed Should add trashState

--- src/main/scala/ExampleAutomatons.scala
+++ src/main/scala/ExampleAutomatons.scala
@@ -18,8 +18,36 @@ case class ExampleNondeterminizedState(
 }
 
 object CSVtoNBA {
-  def parse(csv: String): ExampleNBA = null
-//	TODO
+  def parse(csvFile: String): ExampleNBA = {
+    val bufferedSource = io.Source.fromFile(csvFile)
+    val transitions: Map[Int, AlphabetLetter => List[(Int, Boolean)]] =
+      bufferedSource.getLines.zipWithIndex
+        .map(lineWithIndex => {
+//          val index = lineWithIndex._2
+          val line = lineWithIndex._1
+          val splittedLine = line.split(" ")
+          val index = splittedLine(0).toInt
+
+          val letterToTransition = splittedLine.tail
+            .map(tDescription => {
+              val splittedDescription = tDescription.split(";")
+              val letter = splittedDescription(0).charAt(0)
+              val transitionsOverLetter = splittedDescription.tail
+                .map(t =>
+                  (t.split(",")(0).toInt, t.split(",")(1).charAt(0) == 't'))
+                .toList
+              (letter, transitionsOverLetter)
+            })
+            .toList
+          def f(c: Char) = letterToTransition.find(_._1 == c).orNull._2
+
+          (index, (c: AlphabetLetter) => f(c.c))
+        })
+        .toMap
+    val startingState = ExampleNondeterminizedState(0, transitions)
+    ExampleNBA((startingState, 0) :: Nil, Nil)
+  }
+
 }
 
 case class ExampleNBA(currentStates2: List[(ExampleNondeterminizedState, Int)],
@@ -28,3 +56,6 @@ case class ExampleNBA(currentStates2: List[(ExampleNondeterminizedState, Int)],
                                 prevConditions = prevCondition) {
   override lazy val allStates: List[ExampleNondeterminizedState] = null //TODO
 }
+
+/*
+1 a;2,t;3,f;4 b;2;3;1*/
